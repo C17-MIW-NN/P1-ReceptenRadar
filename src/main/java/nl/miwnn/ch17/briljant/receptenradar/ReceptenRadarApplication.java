dbab6b79b188f78eeed0f90728c7c95d6685ff00
Douwe Jan Hamersma <hamersmadj@hotmail.com>
2025-11-10 11:40:31 +0100

    first commit

---
+++ src/main/java/nl/miwnn/ch17/briljant/receptenradar/ReceptenRadarApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.briljant.receptenradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptenRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptenRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch17/briljant/receptenradar/ReceptenRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.briljant.receptenradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptenRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
